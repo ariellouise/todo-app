c22b9930a5b95ffff1d5e7cddc6731196a45f1dd
ariellouise <60407963+ariellouise@users.noreply.github.com>
2023-07-11 19:38:22 -0400

    added make and model

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,17 +1,38 @@
 import "./App.css";
 
 function App() {
-  const cars = ["Toyata", "Tesla", "Honda", "Ford"];
+  const cars = [
+    { make: "Toyota", model: "Rav4" },
+    { make: "Tesla", model: "One" },
+    { make: "Honda", model: "CRV" },
+    { make: "Ford", model: "F150" },
+  ];
   return (
     <div className="App">
       <h1>Welcome to React!</h1>
       <ul>
         {cars.map((car) => {
-          return <li>{car}</li>;
+          console.log(car);
+          return (
+            <li>
+              {car.make} - {car.model}
+            </li>
+          );
         })}
       </ul>
+      <CarShower />
     </div>
   );
 }
+
+const CarShower = () => {
+  return (
+    <div>
+      <span style={{ fontWeight: "bold" }}>Make: </span>
+      <br />
+      <span style={{ fontWeight: "bold" }}>Model: </span>
+    </div>
+  );
+};
 
 export default App;
