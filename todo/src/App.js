1c590b29a4ef4b31583c1478d92126cda65dbd74
ariellouise <60407963+ariellouise@users.noreply.github.com>
2023-07-11 18:51:35 -0400

    added cars

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 
 function App() {
+  const cars = ["Toyata", "Tesla", "Honda", "Ford"];
   return (
     <div className="App">
       <h1>Welcome to React!</h1>
+      <ul>
+        {cars.map((car) => {
+          return <li>{car}</li>;
+        })}
+      </ul>
     </div>
   );
 }
