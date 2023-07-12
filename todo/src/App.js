a67e964ae2a7c02408bd8540624f314f425a5beb
ariellouise <60407963+ariellouise@users.noreply.github.com>
2023-07-11 20:24:21 -0400

    added useState

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,13 +1,23 @@
+import { useState } from "react";
+
 const App = () => {
-  const addItem = () => {};
+  const [items, setItems] = useState([]);
+  const addItem = (event) => {
+    event.preventDefault();
+    const itemText = event.target.elements.task.value;
+    setItems([...items, itemText]);
+  };
   return (
     <div>
       <h1>To Do List App</h1>
       <form onSubmit={addItem}>
         <label>New To Do Item: </label>
-        <input type="text" />
+        <input type="text" name="task" />
         <button type="submit">Add item</button>
       </form>
+      {items.map((item) => {
+        return <li>{item}</li>;
+      })}
     </div>
   );
 };
