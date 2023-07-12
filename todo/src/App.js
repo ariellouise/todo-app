412591d863e136d2533e103133d3d67b5513ca3e
ariellouise <60407963+ariellouise@users.noreply.github.com>
2023-07-11 20:12:32 -0400

    began todo list

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,36 +1,13 @@
-import "./App.css";
-
-function App() {
-  const cars = [
-    { make: "Toyota", model: "Rav4" },
-    { make: "Tesla", model: "One" },
-    { make: "Honda", model: "CRV" },
-    { make: "Ford", model: "F150" },
-  ];
-  return (
-    <div className="App">
-      <h1>Welcome to React!</h1>
-      <ul>
-        {cars.map((car) => {
-          console.log(car);
-          return (
-            <li>
-              {car.make} - {car.model}
-            </li>
-          );
-        })}
-      </ul>
-      <CarShower />
-    </div>
-  );
-}
-
-const CarShower = (props) => {
+const App = () => {
+  const addItem = () => {};
   return (
     <div>
-      <span style={{ fontWeight: "bold" }}>Make: </span> {props.make}
-      <br />
-      <span style={{ fontWeight: "bold" }}>Model: </span> {props.model}
+      <h1>To Do List App</h1>
+      <form onSubmit={addItem}>
+        <label>New To Do Item: </label>
+        <input type="text" />
+        <button type="submit">Add item</button>
+      </form>
     </div>
   );
 };
