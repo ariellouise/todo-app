dff951e7a3dd6fae574c28a2757311f170f19a34
ariellouise <60407963+ariellouise@users.noreply.github.com>
2023-07-12 18:24:03 -0400

    updated to Max's code

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,13 @@
-//2A Import useState so we can use in #2B
+//#2A Import useState from react so we can use in #2B
 import { useState } from "react";
 
-//#1 Where React project starts (entire App)
+//#1 App where the entire react project starts
 const App = () => {
   //#2B Items and setItems are variable and a function that come from
   //useState (a concept built into react) and items starts out as an empty array
   const [items, setItems] = useState([]);
+  const [itemText, setItemText] = useState("");
+
   // #5B Runs when the form is submitted
   const addItem = (event) => {
     // #6 Prevent the page from refreshing
@@ -18,20 +20,32 @@ const App = () => {
     //In this case we are taking all the existing items, spread them out in a new
     //array then put in the new itemText as another index in that array.
     setItems([...items, itemText]);
+    console.log(items);
   };
+
   return (
     <div>
       <h1>To Do List App</h1>
-      {/* #5 Trigger the addItem function above */}
+      {/* #5A Trigger the addItem function above */}
       <form onSubmit={addItem}>
         <label>New To Do Item: </label>
-        <input type="text" name="task" />
+        <input
+          type="text"
+          name="task"
+          value={itemText}
+          onChange={(event) => {
+            setItemText(event.target.value);
+          }}
+        />
         <button type="submit">Add Item</button>
-        {/* #3 Loop over all the items in our state variable */}
       </form>
+      {/* #3 Loop over all the items in our state variable */}
+      {/* #9 setItems was called, so react auto-magically reruns this loop */}
       {items.map((item, index) => {
         // #4 Show each of the items in the bullet point...
         //but there aren't any items yet so this <li> doesn't show up at first
+        // #10 Because the items array has our new items text in it, it will automagically
+        //render a new li item for us
         return <li key={index}>{item}</li>;
       })}
     </div>
